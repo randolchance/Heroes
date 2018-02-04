be9dfab621830cc2f61791e3b827213555da4e40
Unknown <dan.c.cook@gmail.com>
2018-02-04 13:04:00 -0600

    Initial commit

---
+++ app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+	getHeroes(): Observable<Hero[]> {
+		this.messageService.add('HeroService: fetched heroes');
+		return of(HEROES);
+	}
+
+	getHero(id: number): Observable<Hero> {
+		this.messageService.add(`HeroService: fetched hero id=${id}`);
+  	return of(HEROES.find(hero => hero.id === id));
+	}
+
+  constructor(private messageService: MessageService) { }
+
+}
